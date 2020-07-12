b344ecb71bf7987bb7bbad45e37d79f9f9c5875a
Jomtek <jomtek360@yahoo.fr>
2020-07-12 14:55:45 +0200

    Completed Token class

--- LazenLang/Lexer/Token.cs
+++ LazenLang/Lexer/Token.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace LazenLang.Lexer
 {
+    struct CodePosition
+    {
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+        public CodePosition(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+    }
+
     class Token
     {
+        public string Value { get; set;  }
+        public TokenType Type { get; set; }
+        public CodePosition Pos { get; set; }
+
+        public Token(string value, TokenType type, CodePosition pos)
+        {
+            Value = value;
+            Type = type;
+            Pos = pos;
+        }
     }
 }
