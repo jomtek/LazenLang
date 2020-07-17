7d70804c7935fb7bdfe95e1e9eab008abb24a48e
Jomtek <jomtek360@yahoo.fr>
2020-07-17 21:26:30 +0200

    Added Eat method

---
+++ LazenLang/Parsing/Ast/Block.cs
@@ -0,0 +1,15 @@
+﻿using LazenLang.Parsing.Ast;
+using System.Collections.Generic;
+
+namespace LazenLang.Parser.Ast
+{
+    class Block : Instr
+    {
+        public List<Instr> instructions { get; }
+
+        public Block(List<Instr> instructions)
+        {
+            this.instructions = instructions;
+        }
+    }
+}
--- LazenLang/Parsing/Parser.cs
+++ LazenLang/Parsing/Parser.cs
@@ -1,10 +1,73 @@
-﻿using System;
+﻿using LazenLang.Lexing;
+using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 
 namespace LazenLang.Parser
 {
+    interface ParseErrorContent { }
+
+    struct FailedEatToken : ParseErrorContent
+    {
+        public TokenInfo.TokenType TokenType { get; }
+        public FailedEatToken(TokenInfo.TokenType token)
+        {
+            TokenType = token;
+        }
+    }
+
+    struct NoTokenLeft : ParseErrorContent
+    {}
+
+    class ParseError : Exception
+    {
+        public ParseErrorContent Content;
+        public CodePosition Position;
+
+        public ParseError(ParseErrorContent content, CodePosition position)
+        {
+            Content = content;
+            Position = position;
+        }
+    }
+
     class Parser
     {
+        public List<Token> tokens { get; set; }
+        public CodePosition cursor;
+
+        public Parser(List<Token> tokens)
+        {
+            this.tokens = tokens;
+            cursor = new CodePosition(0, 0);
+        }
+
+        public Token Eat(TokenInfo.TokenType tokenType)
+        {
+            var oldTokens = tokens;
+            Token token;
+
+            try
+            {
+                token = tokens[0];
+                tokens.RemoveAt(0);
+                if (tokens.Count > 0)
+                    cursor = tokens[0].Pos;
+            } catch (IndexOutOfRangeException)
+            {
+                tokens = oldTokens;
+                throw new ParseError(new NoTokenLeft(), new CodePosition(-1, -1));
+            }
+
+            if (token.Type == tokenType)
+            {
+                return token;
+            } else
+            {
+                tokens = oldTokens;
+                throw new ParseError(new FailedEatToken(token.Type), token.Pos);
+            }
+        }
     }
 }
