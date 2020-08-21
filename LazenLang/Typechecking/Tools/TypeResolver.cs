ac0ae4b84c99d06da5abc7800966dbf17280c272
Jomtek <jomtek360@yahoo.fr>
2020-08-21 05:12:29 +0200

    Added type resolver

--- LazenLang/Typechecking/Tools/TypeResolver.cs
+++ LazenLang/Typechecking/Tools/TypeResolver.cs
@@ -2,16 +2,65 @@
 using LazenLang.Parsing.Ast.Expressions;
 using LazenLang.Parsing.Ast.Expressions.Literals;
 using LazenLang.Parsing.Ast.Expressions.OOP;
+using LazenLang.Parsing.Ast.Types;
 using LazenLang.Parsing.Ast.Types.AtomTypes;
 using Microsoft.VisualBasic.CompilerServices;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace LazenLang.Typechecking.Tools
 {
     class TypeResolver
     {
-        public static TypeDesc ResolveType(NativeArray arr)
+        private static TypeDesc AstTypeToTypeDesc(LazenLang.Parsing.Ast.Types.Type type)
+        {
+            if (type is AtomBool) return new BoolType();
+            if (type is AtomChar) return new CharType();
+            if (type is AtomDouble) return new DoubleType();
+            if (type is AtomInt) return new IntType();
+            if (type is AtomString) return new StringType();
+
+            if (type is NativeArrayType)
+            {
+                TypeNode castedType = ((NativeArrayType)type).Type;
+                return new ArrayType(AstTypeToTypeDesc(castedType.Type));
+            }
+
+            if (type is Parsing.Ast.Types.NameType)
+            {
+                var castedType = (Parsing.Ast.Types.NameType)type;
+                return new NameType(castedType.Name);
+            }
+
+            if (type is Parsing.Ast.Types.TypeApp)
+            {
+                var castedType = (Parsing.Ast.Types.TypeApp)type;
+                var generics = new List<TypeDesc>();
+
+                foreach (TypeNode node in castedType.GenericArgs)
+                    generics.Add(AstTypeToTypeDesc(node.Type));
+
+                return new TypeApp((NameType)AstTypeToTypeDesc(castedType.BaseType), generics.ToArray()); 
+            }
+
+            if (type is Parsing.Ast.Types.FuncType)
+            {
+                var castedType = (Parsing.Ast.Types.FuncType)type;
+
+                var domain = new List<TypeDesc>();
+                TypeDesc codomain = AstTypeToTypeDesc(castedType.Codomain.Type);
+
+                foreach (TypeNode node in castedType.Domain)
+                    domain.Add(AstTypeToTypeDesc(node.Type));
+
+                return new FuncType(domain.ToArray(), codomain);
+            }
+
+            throw new ArgumentException();
+        }
+
+        private static TypeDesc ResolveType(NativeArray arr)
         {
             ExprNode[] elements = ((NativeArray)arr).Elements;
 
@@ -39,18 +88,20 @@ namespace LazenLang.Typechecking.Tools
             }
         }
 
-        public static TypeDesc ResolveType(Instanciation instanciation)
+        private static TypeDesc ResolveType(Instanciation instanciation)
         {
             if (instanciation.GenericArgs.Length == 0)
             {
                 return new NameType(instanciation.ClassName);
-            } else
-            {
-                //return new TypeApp(new NameType(instanciation.ClassName), )
             }
+            else
+            {
+                var generics = new List<TypeDesc>();
+                foreach (TypeNode node in instanciation.GenericArgs)
+                    generics.Add(AstTypeToTypeDesc(node.Type));
 
-            return new VoidType();
-            throw new NotImplementedException();
+                return new TypeApp(new NameType(instanciation.ClassName), generics.ToArray());
+            }
         }
 
         public static TypeDesc ResolveType(ExprNode expr)
@@ -65,6 +116,8 @@ namespace LazenLang.Typechecking.Tools
 
             if (value is NativeArray)   return ResolveType((NativeArray)value);
             if (value is Instanciation) return ResolveType((Instanciation)value);
+
+
             return new VoidType();
             throw new NotImplementedException();
         }
